5ee5471814f6bb72f757add12def7b9af60c3a8b
Dmitrii Raven <oguvan4ik@gmail.com>
2021-03-05 19:02:38 +0200

    added logger

---
+++ EStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_4
+{
+    public enum StatusEnum
+    {
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_2_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var start = new Starter();
+            start.Run();
+        }
+    }
+}
